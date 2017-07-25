acd623ed57fa13a126d4fcca160a5635d032b12d
xuld <xuld@vip.qq.com>
2017-07-25 17:08:56 +0800

    修复不内联绝对URL

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var digo = require("digo");
 
 module.exports = function CSSInline(file, options) {
     file.content = file.content.replace(/url\s*\(\s*(?:\s*"([^"]+)"\s*|\s*'([^']+)'\s*|([^)]+))\)/g, (all, url1, url2, url3) => {
-        var p = file.resolve(url1 || url2 || url3);
+        var url = url1 || url2 || url3;
+        if (digo.isAbsoluteUrl(url)) {
+            return all;
+        }
+        var p = file.resolve(url);
         var mimeType = options[digo.getExt(p)];
         if (mimeType) {
             var data = digo.readFileIf(p);
